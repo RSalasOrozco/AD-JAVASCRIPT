e35fdee05b87da752b3bebbdccb7ffe6209424d7
Rodolfo De las Salas <desweb.rs@gmail.com>
2024-08-14 20:51:39 -0500

    actualizacion

--- 02-operadores/04-logicos.js
+++ 02-operadores/04-logicos.js
@@ -1,17 +1,14 @@
 // los operadores logicos son los que nos permiten realizar operaciones logicas, tales como and -- &&, or-- ||, not -- !
 
-//? and -- && funciona cuando dos valores son verdaderos para que el resultado sea verdadero ejemplo: true && true = true
 /*console.log(true && true); //true
 console.log(true && false); // false
 console.log(false && true); // false*/
-
+//? and -- && funciona cuando dos valores son verdaderos para que el resultado sea verdadero ejemplo: true && true = true
 let mayor = true;
 let suscripcion = true;
-
 console.log("operador AND --", mayor && suscripcion);
 
-//? or -- || funciona cuando almenos uno de los  valores es verdadero, para que el resultado sea verdadero ejemplo: true || false = true
-
+//? or -- || funciona cuando almenos uno de los  valores es verdadero, para que el resultado sea true  ejemplo: true || false = true
 console.log("operador OR --", mayor || suscrito);
 
 //? not -- ! funciona cuando el valor es verdadero para que el resultado sea falso ejemplo: !true = false
--- 02-operadores/05-falsy.js
+++ 02-operadores/05-falsy.js
@@ -1,1 +1,25 @@
-// short-circuit
+// short-circuit-
+//! EVALUA EN FALSO
+// false - FALSO
+// 0 - CERO
+// "" O '' - STRING VACIO
+// null - VALOR NULO
+// undefined - VALOR INDEFINIDO
+// NaN - NOT A NUMBER
+
+//? Ejemplo con OR -- || funciona cuando almenos uno de los  valores es verdadero, para que el resultado sea true
+let nombre = "Desweb";
+let username = nombre || "Anonimo";
+console.log(username);
+
+//? Ejemplo con AND -- && funciona cuando dos valores son verdaderos para que el resultado sea verdadero
+function fn1() {
+  console.log("soy fn1");
+  return false;
+}
+function fn2() {
+  console.log("soy fn2");
+  return true;
+}
+// and nos ayudara si tenemos dos funciones y queremos que nos retorne una funcion, por ejemplo si queremos que si la fn1 es verdadera y la fn2 es falsa, nos retorne la fn2
+let x = fn1() && fn2();
