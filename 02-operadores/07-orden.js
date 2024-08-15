59afebe0adf8ae92a2301377051cc6ec9d2a8767
Rodolfo De las Salas <desweb.rs@gmail.com>
2024-08-14 21:14:23 -0500

    add code

---
+++ 02-operadores/07-orden.js
@@ -0,0 +1,4 @@
+// estableceremos el orden de las operaciones a nuestro gusto
+// si deseamos establecer un orden de ejecucion debemos hacerlo dentro de parentesis, dentro de los parentesis debe estar la operacion o la funciones que queremos que se ejecute primero
+let resultado = 8 / (2 * (2 + 2));
+console.log(resultado);
---
+++ 02-operadores/08-ternarios.js
@@ -0,0 +1,5 @@
+// Operadores ternarios funcionan como un if-else
+//? por ejemplo: expresion ? valor1 : valor2 se lee asi, si la expresion es verdadera, retornara el valor1, de lo contrario retornara el valor2
+let edad = 25;
+let acceso = edad >= 17 ? "Puede ingresar" : "No puede ingresar";
+console.log(acceso);
